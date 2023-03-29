9ab245a75e5caa998ccfeeb970074c02fc297ced
mathjken <ogujohnkennedy@gmail.com>
2023-03-29 02:33:33 +0000

    task 3 to 10

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,10 @@
+
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Johnkennedy Ogu<https://github.com/Mathjken>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((p) => (p !== undefined ? p.startsWith(startString) : ''))
+    .map((p) => (p !== undefined ? p.slice(startString.length) : ''))
+    .join('-');
+};
+
+export default cleanSet;
